822bd127b1baacb500ab831e942baa70ea96b7bd
Chandan Pandey <cpandey05@gmail.com>
2025-08-15 20:48:28 +0530

    Fixed method

--- src/main/java/com/techeazy/monitor_traffic/MonitorTrafficApplication.java
+++ src/main/java/com/techeazy/monitor_traffic/MonitorTrafficApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MonitorTrafficApplication {
 	//Trying Git locally 
-	public static void main(String[] args) {
+	ublic static void main(String[] args) {
 		SpringApplication.run(MonitorTrafficApplication.class, args);
 	}
 
