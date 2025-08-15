5f76d417e35f60630ea3ed6194d4142f4f62571a
Chandan Pandey <cpandey05@gmail.com>
2025-08-15 20:19:52 +0530

    Initial version

---
+++ src/test/java/com/techeazy/monitor_traffic/MonitorTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeazy.monitor_traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorTrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
