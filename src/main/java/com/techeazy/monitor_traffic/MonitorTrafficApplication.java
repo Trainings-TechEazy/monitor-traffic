45c5b6f1d03e59f2efedd75b835f93fdddd391e1
Trainings-TechEazy <trainings.techeazyconsulting@gmail.com>
2025-08-20 20:34:36 +0530

    Update MonitorTrafficApplication.java

--- src/main/java/com/techeazy/monitor_traffic/MonitorTrafficApplication.java
+++ src/main/java/com/techeazy/monitor_traffic/MonitorTrafficApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MonitorTrafficApplication {
 
-	//Trying Git locally -corrected , analytics branch,mage enhanced 
+	//Trying Git locally -corrected , analytics branch,mage enhanced , Image -20 Aug
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorTrafficApplication.class, args);
